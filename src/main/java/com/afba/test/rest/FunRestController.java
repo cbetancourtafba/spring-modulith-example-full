c233a45c4597dbefbdf8abd867c1b525db08514b
Cristian Betancourt <cbetancourt@AFBA.COM>
2024-09-14 20:41:05 -0400

    testing rules #1

--- src/main/java/com/afba/test/rest/FunRestController.java
+++ src/main/java/com/afba/test/rest/FunRestController.java
@@ -9,7 +9,21 @@ public class FunRestController {
   // expose "/" that return "Hello World"
   @GetMapping("/")
   public String sayHello() {
-    
+
     return "Hello World!!";
+  }
+
+  /// Example 1: Null Pointer Dereference
+  public void processString(String input) {
+    if (input.length() > 5) { // Potential null pointer dereference
+      System.out.println(input.toUpperCase());
+    }
+  }
+
+  // Example 2: Unused Variable
+  public int calculateSum(int a, int b) {
+    int result = a + b;
+    int unusedVariable = 10; // Unused variable
+    return result;
   }
 }
